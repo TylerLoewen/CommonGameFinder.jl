8791c602160c5ae0d7c6900a1bb6ad9045d7b380
Tyler Loewen <info@tylerloewen.me>
2021-06-08 22:33:22 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/CommonGameFinder.jl
@@ -0,0 +1,5 @@
+module CommonGameFinder
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CommonGameFinder
+using Test
+
+@testset "CommonGameFinder.jl" begin
+    # Write your tests here.
+end
