d79c3cf02b43a68963055b65390a95d5d7319c79
Tyler Loewen <info@tylerloewen.me>
2021-06-13 12:40:32 -0500

    Add ability to get ids of shared games between multiple accounts

--- src/CommonGameFinder.jl
+++ src/CommonGameFinder.jl
@@ -1,5 +1,6 @@
 module CommonGameFinder
 
+using Base: Integer
 using HTTP
 using JSON3
 using Memento
@@ -9,10 +10,8 @@ const LOGGER = getlogger(@__MODULE__)
 const secrets = YAML.load_file("secrets.yaml")
 const format = "json"
 
-export get_owned_games
-
 include("main.jl")
 
-get_owned_games()
+main()
 
 end # module
--- src/main.jl
+++ src/main.jl
@@ -1,10 +1,35 @@
-function get_owned_games()
+calculate_shared_games(game_ids::Tuple{Vararg{Set{Integer}}}) = intersect(game_ids...)
+
+function get_owned_game_ids(steam_account_id::Integer)::Set{Integer}
 
     api_key = secrets["API_KEY"]
-    steam_account_id = secrets["STEAM_ACCOUNT_ID"]
 
     r = HTTP.request("GET", "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$api_key&steamid=$steam_account_id&format=$format")
-    println(r.status)
-    println(JSON3.read(r.body))
+    info(LOGGER, "Status: $(r.status)" )
+    response = JSON3.read(r.body).response
+
+    owned_games = Set{Integer}()
+    for game in response.games
+        push!(owned_games, game.appid)
+    end
+
+    return owned_games
+
+end
+
+function main()
+
+    acc_1 = get_owned_game_ids(Integer(secrets["STEAM_ACCOUNT_ID_1"]))
+    acc_2 = get_owned_game_ids(Integer(secrets["STEAM_ACCOUNT_ID_2"]))
+    acc_3 = get_owned_game_ids(Integer(secrets["STEAM_ACCOUNT_ID_3"]))
+    acc_4 = get_owned_game_ids(Integer(secrets["STEAM_ACCOUNT_ID_4"]))
+
+    common_games = calculate_shared_games((acc_1, acc_2, acc_3, acc_4))
+
+    println("Shared games:")
+
+    for game in common_games
+        println(game)
+    end
 
 end
