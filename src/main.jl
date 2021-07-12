e20b640680f9d0247ba4866e6223b209be1d04a6
Tyler Loewen <info@tylerloewen.me>
2021-07-11 23:30:17 -0500

    Add ability to get summary of steam accounts and get names of friends

--- src/main.jl
+++ src/main.jl
@@ -22,6 +22,16 @@ function get_friend_list(steamid::Integer)::JSON3.Array
 
 end
 
+function get_player_summaries(steamids::Vector{Integer})::JSON3.Array
+
+    api_key = secrets["API_KEY"]
+
+    r = HTTP.request("GET", "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=$api_key&steamids=$(join(steamids, ","))")
+    info(LOGGER, "Status: $(r.status)" )
+    player_summaries = JSON3.read(r.body).response.players
+    return player_summaries
+end
+
 common_games_appids(games::Tuple{Vararg{JSON3.Object}})::Set{Integer} = intersect(map(games_appids, games)...)
 
 function games_appids(games::JSON3.Object)::Set{Integer}
@@ -35,7 +45,7 @@ function games_appids(games::JSON3.Object)::Set{Integer}
 
 end
 
-function friend_steamids(friend_list::JSON3.Array)
+function friend_steamids(friend_list::JSON3.Array)::Vector{Integer}
 
     friend_steamids = Vector{Integer}()
     for friend in friend_list
@@ -43,6 +53,17 @@ function friend_steamids(friend_list::JSON3.Array)
     end
 
     return friend_steamids
+
+end
+
+function friend_names(friend_list::JSON3.Array)::Vector{AbstractString}
+
+    friend_names = Vector{AbstractString}()
+    for friend in friend_list
+        push!(friend_names, friend.personaname)
+    end
+
+    return friend_names
 
 end
 
@@ -75,4 +96,14 @@ function main()
         println(friend_steamid)
     end
 
+
+    # Get list of friends names
+    friends = get_friend_list(Integer(secrets["STEAM_ACCOUNT_ID_1"]))
+    friend_steamids_list = friend_steamids(friends)
+    r =  get_player_summaries(friend_steamids_list)
+    names = friend_names(r)
+
+    for name in names
+        println(name)
+    end
 end
