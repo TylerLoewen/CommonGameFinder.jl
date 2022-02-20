af1795a10561d36a5b125b0d3829b7d8fae0cb03
Tyler Loewen <tyler.loewen@invenia.ca>
2022-02-20 10:35:20 -0600

    Remove all business logic code

--- src/CommonGameFinder.jl
+++ src/CommonGameFinder.jl
@@ -28,32 +28,4 @@ owned_games_2 = get_owned_games(Int64(SECRETS["STEAM_ACCOUNT_ID_2"]))
 owned_games_3 = get_owned_games(Int64(SECRETS["STEAM_ACCOUNT_ID_3"]))
 owned_games_4 = get_owned_games(Int64(SECRETS["STEAM_ACCOUNT_ID_4"]))
 
-# common_appids = get_common_games_appids(
-#     owned_games_1, owned_games_2, owned_games_3, owned_games_4
-# )
-
-# println("Common game id's:")
-# for appid in common_appids
-#     println(appid)
-# end
-
-# # Get list of friends steamids
-# friends = get_friend_list(Int64(SECRETS["STEAM_ACCOUNT_ID_1"]))
-
-# friend_steamids_list = friend_steamids(friends)
-
-# println("Account 1's friends:")
-# for friend_steamid in friend_steamids_list
-#     println(friend_steamid)
-# end
-
-# # Get list of friends names
-# player_summaries =  get_player_summaries(friend_steamids_list)
-# names = friend_names(player_summaries)
-
-# println("Account 1's friend names:")
-# for name in names
-#     println(name)
-# end
-
 end # module
