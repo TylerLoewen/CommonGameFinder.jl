2076bd91ffca2435cd444af9b3ecd0d5aa3ef6f8
Tyler Loewen <info@tylerloewen.me>
2022-02-21 12:32:37 -0600

    Refactor (#12)
    * Add new Games and Game types. Refactor get_owned_games to use new types
    
    * Remove all business logic code
    
    * New modules and structure. Complete owned_games stack working
    
    * Remove ResultType
    
    * Add new Friend and Friends type. Add method to build a users list of Friends
    
    * Improve createfriends code
    
    * Improve createGames code
    
    * testing

---
+++ src/Client.jl
@@ -0,0 +1,47 @@
+module Client
+
+using HTTP
+using JSON3
+using YAML
+
+using ..Service
+using ..Model
+
+const SECRETS = YAML.load_file("./secrets.yaml")
+const RESPONSE_FORMAT = "json"
+
+function getOwnedGames(steamid::Int64)
+    api_key = SECRETS["API_KEY"]
+
+    res = HTTP.request(
+        "GET",
+        "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$api_key&steamid=$steamid&format=$RESPONSE_FORMAT&include_appinfo=true",
+    )
+
+    # We only care about the body, not any headers
+    return JSON3.read(res.body).response.games
+end
+
+function getFriendList(steamid::Int64)
+    api_key = SECRETS["API_KEY"]
+
+    r = HTTP.request(
+        "GET",
+        "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=$api_key&steamid=$steamid&relationship=friend",
+    )
+
+    return JSON3.read(r.body).friendslist.friends
+end
+
+function getPlayerSummaries(steamids::Vector{Int64})
+    api_key = SECRETS["API_KEY"]
+
+    r = HTTP.request(
+        "GET",
+        "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=$api_key&steamids=$(join(steamids, ","))",
+    )
+
+    return JSON3.read(r.body).response.players
+end
+
+end # module
---
+++ src/Model.jl
@@ -0,0 +1,38 @@
+module Model
+
+export Game, Games, Friend, Friends
+
+struct Game
+    appid::Int64
+    name::String
+    playtime_forever::Int64 # Minutes
+end
+
+Game() = Game(0, "", 0)
+Game(appid, name) = Game(appid, name, 0)
+
+struct Games
+    account_id::Int64 # Account that owns the games
+    games::Dict{Int64,Game}
+end
+
+Games() = Games(0, Dict{Int64,Game}())
+Games(account_id) = Games(account_id, Dict{Int64,Game}())
+
+struct Friend
+    steamid::Int64
+    personaname::String
+end
+
+Friend() = Friend(0, "")
+Friend(steamid) = Friend(steamid, "")
+
+struct Friends
+    account_id::Int64 # Account that these friends belong to
+    friends::Vector{Friend}
+end
+
+Friends() = Friends(0, Vector{Friend}())
+Friends(account_id) = Friends(account_id, Vector{Friend}())
+
+end # module
